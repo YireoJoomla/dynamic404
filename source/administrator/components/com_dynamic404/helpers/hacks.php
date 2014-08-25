e6c5744574687bf2732aa9d919798d1f55e542d1
Jisse Reitsma <jisse@yireo.com>
2014-08-25 17:46:38 +0200

    Pending changes

---
+++ source/administrator/components/com_dynamic404/helpers/hacks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Joomla! component Dynamic404
+ *
+ * @author      Yireo (http://www.yireo.com/)
+ * @package     Dynamic404
+ * @copyright   Copyright (C) 2014 Yireo (http://www.yireo.com/)
+ * @license     GNU Public License (GPL) version 3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ * @link        http://www.yireo.com/
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+$hacks = array(
+    '../proc/self/environ_',
+    '/../../../../../../',
+);
